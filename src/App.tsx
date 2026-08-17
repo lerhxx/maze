80b5238550a4a7bfb9342c8b9a159f371d2e8db6
hexuehuan <hexuehuan@kilox.cn>
2026-08-17 15:33:42 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Canvas } from '@react-three/fiber';
+import './App.css'
+
+function App() {
+  return (
+    <Canvas>
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial />
+      </mesh>
+    </Canvas>
+  )
+}
+
+export default App
